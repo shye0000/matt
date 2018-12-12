63ee53251199428fc9f5f85ae990bd1c803d9155
matt.li <matt.li@famoco.com>
2018-12-12 17:10:18 +0100

    left hobbies zone debug

--- src/components/left/components/hobbies/index.js
+++ src/components/left/components/hobbies/index.js
@@ -19,18 +19,18 @@ const Hobbies = ({ hobbies }) => (
     <Row gutter={20}>
       <Col span={12}>
         <div className="content">
-          {hobbies.filter((hobby, idx) => idx <= hobbies.length / 2 - 1)
+          {hobbies.filter((hobby, idx) => idx < hobbies.length / 2)
             .map(hobby => (
-              <div>{hobby}</div>
+              <div key={hobby}>{hobby}</div>
             ))
           }
         </div>
       </Col>
       <Col span={12}>
         <div className="content">
-          {hobbies.filter((hobby, idx) => idx > hobbies.length / 2 - 1)
+          {hobbies.filter((hobby, idx) => idx >= hobbies.length / 2)
             .map(hobby => (
-              <div>{hobby}</div>
+              <div key={hobby}>{hobby}</div>
             ))
           }
         </div>
